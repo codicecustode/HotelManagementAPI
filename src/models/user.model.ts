547c786d1799ae085108cb8614412cc8944540d0
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-02 00:05:45 +0530

    Implement user model with authentication methods and validation, update package dependencies

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,9 @@
-interface User {
+import { Schema, Document } from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+
+interface IUser extends Document {
   userName: string;
   fullName: string;
   email: string;
@@ -24,5 +29,120 @@ interface User {
   generateEmailVerificationToken(): string;
   generateResetPasswordToken(): string;
   comparePassword(password: string): boolean;
-  }
+}
 
+const userSchema = new Schema<IUser>({
+  userName: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+    trim: true,
+  },
+  fullName: {
+    type: String,
+    required: [true, 'Full name is required'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required:  [true, 'Email is required'],
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    minlength: [6, 'Password must be at least 6 characters'],
+    required: [true, 'Password is required'],
+    select: false,
+  },
+  phone: {
+    type: String,
+  },
+  avtar: {
+    type: String
+  },
+  gender:{
+    type: String,
+    enum: ['male', 'female'],
+  },
+  dob: {
+    type: Date
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  status: {
+    type: String,
+    enum: ['register', 'login', 'logout', 'blocked'],
+    default: 'register',
+  },
+  resetPasswordToken: {
+    type: String,
+  },
+  resetPasswordExpires: {
+    type: Date,
+  },
+  emailVerificationToken: {
+    type: String,
+  },
+  emailVerificationExpires: {
+    type: Date,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },  
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+//
+userSchema.pre('save', function (next) {
+  if(this.userName){
+    this.userName = this.userName.toLowerCase().trim().replace(/\s/g, '');
+  }
+  next();
+});
+//middleware to hash password before saving
+userSchema.pre('save', async function (next) {
+  if(this.isModified('password')){
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+//custom methods
+userSchema.methods.comparePassword = async function (password: string): Promise<boolean> {
+  return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAuthToken = function (): string {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET_KEY as string, { expiresIn: process.env.JWT_ACCESS_EXPIRES as string });
+  return token;
+}
+
+userSchema.methods.generateRefrshToken = function (): string {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET_KEY as string, { expiresIn: process.env.JWT_REFRESH_EXPIRES as string });
+  return token;
+}
+
+userSchema.methods.generateEmailVerificationToken = function (): string {
+  const token = crypto.randomBytes(20).toString('hex');
+  this.emailVerificationToken = crypto.createHash('sha256').update(token).digest('hex');
+  this.emailVerificationExpires = new Date(Date.now() + 24 * 60 * 60 * 1000);
+  return token;
+}
+
+userSchema.methods.generateResetPasswordToken = function (): string {
+  const token = crypto.randomBytes(20).toString('hex');
+  this.resetPasswordToken = crypto.createHash('sha256').update(token).digest('hex');
+  this.resetPasswordExpires = new Date(Date.now() + 24 * 60 * 60 * 1000);
+  return token;
+}
