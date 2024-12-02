daa22af9d3bf402b0522defdc48945a316736073
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-02 18:28:36 +0530

    Update server configuration and add body-parser middleware for JSON handling

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
 import app from './src/app/index';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT 
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import bodyParser from 'body-parser';
 
 import connectMongoDB from '../database/connect.mongo.db';
-
+import { authRouter } from '../routes/auth.route';
 dotenv.config();
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //connecti to database
 try{
@@ -14,6 +17,10 @@ try{
   console.error('MongoDB connection failed', error);
   process.exit(1);
 }
+
+
+//set API routes
+app.use('/api/v1', authRouter);
 
 
 
