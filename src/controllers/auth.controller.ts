a754ec2a746a5ce4f8bfcf55539af2efd6b9004c
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-03 21:22:19 +0530

    Enhance user registration by adding validation for required fields and image upload; update response to include created user details

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { IUser } from '../models/user.model';
+import { User } from '../models/user.model';
 export const registerUser = async (req: Request, res: Response) => {
   try {
     const { 
@@ -14,12 +15,44 @@ export const registerUser = async (req: Request, res: Response) => {
       role
     }: IUser = req.body;
 
-    console.log(req);
-    return res.status(200).json({
-      request: req.body,
-      message: 'User created successfully',
+    if(!userName || !fullName || !email || !password || !phone || !address || !role){
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({ message: 'Please upload an image' });
+    }
+
+    const isUserExist = await User.findOne({
+      $or: [{ email }, { phone }, { userName }],
     });
-  } catch (error) {
-    console.error(error);
+
+    if (isUserExist) {
+      return res.status(400).json({ message: 'User already exist' });
+    }
+
+    const user = new User({
+      userName,
+      fullName,
+      email,
+      password,
+      phone,
+      address,
+      gender,
+      dob,
+      role,
+      avtar: req.file?.filename,
+    });
+
+    await user.save();
+
+    return res.status(200).json({
+      message: 'User created successfully',
+      createdUser: user,
+    });
+  } catch (error: any) {
+    return res.status(500)
+    .json({ message: 'Internal Server Error', error: error.message });
   }
 }
+
