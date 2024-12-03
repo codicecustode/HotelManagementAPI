7df3a66465b5ca042dc572d01fd183917fd49c4e
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-03 23:22:22 +0530

    Implement user login functionality and refactor registerUser method

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
 import { IUser } from '../models/user.model';
 import { User } from '../models/user.model';
-export const registerUser = async (req: Request, res: Response) => {
+const registerUser = async (req: Request, res: Response) => {
   try {
-    const { 
+    const {
       userName,
       fullName,
       email,
@@ -15,7 +15,7 @@ export const registerUser = async (req: Request, res: Response) => {
       role
     }: IUser = req.body;
 
-    if(!userName || !fullName || !email || !password || !phone || !address || !role){
+    if (!userName || !fullName || !email || !password || !phone || !address || !role) {
       return res.status(400).json({ message: 'All fields are required' });
     }
 
@@ -41,7 +41,7 @@ export const registerUser = async (req: Request, res: Response) => {
       gender,
       dob,
       role,
-      avtar: req.file? `uploads/${req.file.filename}` : 'avtar.png',
+      avtar: req.file ? `uploads/${req.file.filename}` : 'avtar.png',
     });
 
     await user.save();
@@ -52,7 +52,67 @@ export const registerUser = async (req: Request, res: Response) => {
     });
   } catch (error: any) {
     return res.status(500)
-    .json({ message: 'Internal Server Error', error: error.message });
+      .json({ message: 'Internal Server Error', error: error.message });
   }
 }
+
+const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const { userType } = req.query;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and password are required' });
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  if (userType === 'admin') {
+    if (user.role !== 'admin') {
+      return res.status(401).json({ message: 'Unauthorized user' });
+    }
+  }
+
+  if (user.status === 'blocked') {
+    return res.status(401).json({ message: 'User is blocked' });
+  }
+
+  const isPasswordMatch = user.comparePassword(password);
+  if (!isPasswordMatch) {
+    return res.status(401).json({ message: 'Invalid password' });
+  }
+
+  const loggedUser = User.findByIdAndUpdate(
+    user._id,
+    {
+      $set: {
+        status: 'login',
+        updatedAt: new Date(),
+      }
+    },
+    { new: true },
+  )
+
+  const token = user.generateAuthToken();
+  const refreshToken = user.generateRefrshToken();
+
+  const options = {
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  };
+
+  return res.status(200)
+    .cookie('token', token, options)
+    .json({ message: 'User logged in successfully', user: loggedUser });
+}
+
+export {
+  registerUser,
+  loginUser
+}
+
+
 
