aa2c0e299f98df40d37f16ec569d8f8d9739e258
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-05 15:41:45 +0530

    Refactor registerUser method to use RequestHandler type and streamline response handling

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express';
+import { Request, Response, RequestHandler } from 'express';
 import { IUser } from '../models/user.model';
 import { User } from '../models/user.model';
-const registerUser = async (req: Request, res: Response) => {
+const registerUser: RequestHandler = async (req: Request, res: Response) => {
   try {
     const {
       userName,
@@ -16,11 +16,13 @@ const registerUser = async (req: Request, res: Response) => {
     }: IUser = req.body;
 
     if (!userName || !fullName || !email || !password || !phone || !address || !role) {
-      return res.status(400).json({ message: 'All fields are required' });
+      res.status(400).json({ message: 'All fields are required' });
+      return
     }
 
     if (!req.file) {
-      return res.status(400).json({ message: 'Please upload an image' });
+      res.status(400).json({ message: 'Please upload an image' });
+      return
     }
 
     const isUserExist = await User.findOne({
@@ -28,7 +30,8 @@ const registerUser = async (req: Request, res: Response) => {
     });
 
     if (isUserExist) {
-      return res.status(400).json({ message: 'User already exist' });
+      res.status(400).json({ message: 'User already exist' });
+      return
     }
 
     const user = new User({
@@ -46,15 +49,17 @@ const registerUser = async (req: Request, res: Response) => {
 
     await user.save();
 
-    return res.status(200).json({
+    res.status(200).json({
       message: 'User created successfully',
       createdUser: user,
     });
+    return
   } catch (error: any) {
-    return res.status(500)
+    res.status(500)
       .json({ message: 'Internal Server Error', error: error.message });
+    return
   }
-}
+};
 
 const loginUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -70,7 +75,7 @@ const loginUser = async (req: Request, res: Response) => {
     return res.status(404).json({ message: 'User not found' });
   }
 
-  if(user.verified === false) {
+  if (user.verified === false) {
     return res.status(400).json({ message: 'User not verified' });
   }
 
