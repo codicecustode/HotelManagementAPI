720aefa4bd0434dcc31ff36c0ea476207f50eb2a
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-07 14:46:45 +0530

    Enhance authentication middleware: refactor token handling, add refresh token validation, and improve error responses

--- src/middleware/user.authentication.ts
+++ src/middleware/user.authentication.ts
@@ -4,37 +4,70 @@ import { User } from '../models/user.model'; // Adjust the path as necessary
 
 const isUserAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
 
-  const authHeader = req.headers['Authorization'];
-  const token = Array.isArray(authHeader) ? authHeader[0].split(' ')[1] : authHeader?.split(' ')[1];
-
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
   if (!token) {
-    return res.status(401).json({ message: 'Access denied. No token provided' });
+    res.status(401).json({ message: 'Access denied. No token provided' });
+    return;
   }
-
   try {
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
     const user = await User.findById((decodedToken as jwt.JwtPayload)._id);
 
     if (!user) {
-      return res.status(401).json({ message: 'User not found' });
+      res.status(401).json({ message: 'User not found' });
+      return;
     }
 
     if (user.status === 'blocked') {
-      return res.status(401).json({ message: 'User blocked' });
-    }
 
-    if (user.status === 'login') {
-      req.user = user;
+      res.status(401).json({ message: 'User blocked' });
+      return;
     }
+    //   console.log(user.status);
+    //   if (user.status === 'login') {
+    //     console.log('not ok');
+    //     req.user = user;
+    //   }
+    // }
+    req.user = user;
   }
   catch (error) {
-      return res.status(401).json({ message: 'Invalid token' });
-    }
-    next();
+    console.log(error);
+    res.status(401).json({ message: 'Invalid token' });
+    return;
+  }
+  console.log(req.user);
+  next();
+};
+
+const isRefreshTokenValid = async (req: Request, res: Response, next: NextFunction) => {
+
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) {
+    res.status(401).json({ message: 'Access denied. No token provided' });
+    return;
   }
 
+  try {
+    const decodedToken = jwt.verify(refreshToken, process.env.JWT_SECRET_KEY as string);
+    const user = await User.findById((decodedToken as jwt.JwtPayload)._id);
+    if (!user) {
+      res.status(401).json({ message: 'User not found' });
+      return;
+    }
+    req.user = user;
+  }
+  catch (error) {
+    res.status(401).json({ message: 'Invalid token' });
+    return;
+  }
+  next();
+}
 
 
-  export { 
-    isUserAuthenticated 
-  };
+
+export {
+  isUserAuthenticated,
+  isRefreshTokenValid
+};
