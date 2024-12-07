e35d84860bff2015e78e6cbc8f302de924c81022
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-07 14:47:12 +0530

    Enhance authentication routes: add email verification and refresh token endpoints, and integrate authentication middleware

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,10 @@
 import express from 'express';
-import { registerUser } from '../controllers/auth.controller';
+import { 
+  registerUser,
+  sendEmailVerificationLink,
+  refreshAccessToken
+ } from '../controllers/auth.controller';
+import { isUserAuthenticated, isRefreshTokenValid } from '../middleware/user.authentication';
 import upload from '../middleware/user.avtar.upload';
 import { sendEmail } from '../utils/send.email';
 const router = express.Router();
@@ -9,7 +14,12 @@ const router = express.Router();
 router.post('/send-email', )
 
 router.post('/register',upload.single('avtar'), registerUser);
+router.post('/send-verification-email', isUserAuthenticated, sendEmailVerificationLink)
 
+
+
+
+router.route('/refresh-token').get(isRefreshTokenValid, refreshAccessToken);
 
 
 export { router as authRouter };
