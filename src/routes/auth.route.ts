938babdc20d985431db3961357983f6960d9d5a4
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-09 15:34:58 +0530

    Add login, forgot password, and reset password routes to authentication API

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -5,9 +5,12 @@ import {
   refreshAccessToken,
   verifyEmail,
   logoutUser,
-  changePassword
+  changePassword,
+  forgotPassword,
+  resetPassword,
+  loginUser
  } from '../controllers/auth.controller';
-import { isUserAuthenticated, isRefreshTokenValid } from '../middleware/user.authentication';
+import { isUserAuthenticated, isRefreshTokenValid, isPasswordResetTokenValid } from '../middleware/user.authentication';
 import upload from '../middleware/user.avtar.upload';
 import { sendEmail } from '../utils/send.email';
 const router = express.Router();
@@ -16,13 +19,18 @@ const router = express.Router();
 //test route
 
 router.route('/register').post(upload.single('avtar'), registerUser);
+router.route('/login-user').post(loginUser);
 router.route('/logout-user').post(isUserAuthenticated, logoutUser);
+
+
 router.route('/change-password').post(isUserAuthenticated, changePassword);
+router.route('/forgot-password').post(forgotPassword);
+router.route('/reset-password/:resetPasswordToken').post(isPasswordResetTokenValid, resetPassword);
+
+
 
 router.route('/send-verification-email').post(isUserAuthenticated, sendEmailVerificationLink);
 router.route('/verify-email/:token').post(isUserAuthenticated, verifyEmail);
-
-
 
 
 router.route('/refresh-token').get(isRefreshTokenValid, refreshAccessToken);
