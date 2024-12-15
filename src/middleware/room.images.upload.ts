57d15f0c549574eb49096be3b066ed403857656e
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-15 08:36:18 +0530

    Refactor image upload middleware to use dynamic upload directory and improve filename handling

--- src/middleware/room.images.upload.ts
+++ src/middleware/room.images.upload.ts
@@ -1,21 +1,26 @@
 import multer from 'multer';
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const uploadDir = './uploads/room.images';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-if(!fs.existsSync(uploadDir)){
-  fs.mkdirSync(uploadDir)
+const uploadDir = path.join(__dirname, '../uploads/rooms-images');
+console.log("s", uploadDir)
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true })
 }
-
-
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/uploads/room.images')
+    cb(null, uploadDir)
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = 'room-image' + '-' + Date.now()
-    cb(null, file.originalname.replace(file.originalname, uniqueSuffix))
+    const uniqueSuffix = 'room-image-' + Date.now();
+    const fileExtension = path.extname(file.originalname);
+    cb(null, uniqueSuffix + fileExtension);
   }
 })
 
@@ -23,10 +28,10 @@ const upload = multer({
   storage,
   limits: { fileSize: 1000000 },
   fileFilter: (req, file, cb) => {
-    if(file.mimetype === 'image/png' || file.mimetype==='image/jpeg'){
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
       cb(null, true)
-    }else{
-      cb(null, false)
+    } else {
+      cb(new Error('Only .png and .jpeg format allowed!'))
     }
   }
 })
