e1c44b04a95407c9b694e32f2ddf508660585b02
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-17 00:47:50 +0530

    Add authentication and authorization middleware to room creation route

--- src/routes/room.route.ts
+++ src/routes/room.route.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 
 import { createRoom } from '../controllers/room.controller';
 import { uploadimages } from '../middleware/room.images.upload';
+import { isUserAuthenticated, isUserAdmin } from '../middleware/user.authentication';
 
 
 const router = Router();
 
-router.route('/create-room').post(uploadimages.array('roomImages',2), createRoom);
+router.route('/create-room').post(isUserAuthenticated, isUserAdmin, uploadimages.array('roomImages',2), createRoom);
 
 export { router as roomRouter };
